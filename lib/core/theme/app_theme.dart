f50081975353614f6e5aec68a4d7b9dea0d2c580
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-21 10:44:08 +0600

    feat: added theme

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'package:shoppe/core/theme/app_color_palette.dart';
+import 'package:shoppe/core/constants/app_colors.dart';
+import 'package:shoppe/core/theme/app_elevation_theme.dart';
+
+ThemeData get lightTheme => ThemeData(
+  useMaterial3: true,
+  extensions: <ThemeExtension<dynamic>>[
+    AppColorPalette(
+      primary: AppColors.primary,
+      black: AppColors.black,
+      yellow: AppColors.yellow,
+      periwinkle: AppColors.periwinkle,
+      lightPeriwinkle: AppColors.lightPeriwinkle,
+      pink100: AppColors.pink100,
+      pink200: AppColors.pink200,
+      pink300: AppColors.pink300,
+      pink400: AppColors.pink400,
+      pink500: AppColors.pink500,
+      white: AppColors.white,
+      grey20: AppColors.grey20,
+      backgroundGrey: AppColors.backgroundGrey,
+    ),
+    AppElevationTheme.light(),
+  ],
+);
+
+ThemeData get darkTheme => ThemeData(
+  useMaterial3: true,
+  extensions: <ThemeExtension<dynamic>>[
+    AppColorPalette(
+      primary: AppColors.primary,
+      black: AppColors.black,
+      yellow: AppColors.yellow,
+      periwinkle: AppColors.periwinkle,
+      lightPeriwinkle: AppColors.lightPeriwinkle,
+      pink100: AppColors.pink100,
+      pink200: AppColors.pink200,
+      pink300: AppColors.pink300,
+      pink400: AppColors.pink400,
+      pink500: AppColors.pink500,
+      white: AppColors.white,
+      grey20: AppColors.grey20,
+      backgroundGrey: AppColors.backgroundGrey,
+    ),
+    AppElevationTheme.dark(),
+  ],
+);
