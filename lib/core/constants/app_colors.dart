6dfbc84e84b504a74cbbb48b15fd59c29659fb36
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-21 08:50:16 +0600

    feat: added app color (this is the first task)

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const primary = Color(0xFF004CFF);
+  static const black = Color(0xFF000000);
+  static const yellow = Color(0xFFECA61B);
+  static const periwinkle = Color(0xFFCCD9F9);
+  static const lightPeriwinkle = Color(0xFFE5EBFC);
+  static const pink100 = Color(0xFFFFEBEB);
+  static const pink200 = Color(0xFFF1AEAE);
+  static const pink300 = Color(0xFFD97474);
+  static const pink400 = Color(0xFFEC4E4E);
+  static const pink500 = Color(0xFFFB2E62);
+  static const white = Color(0xFFFFFFFF);
+  static const grey20 = Color(0xFFD2D2D2);
+  static const backgroundGrey = Color(0xFFF8F8F8);
+}
