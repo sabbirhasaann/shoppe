c656144b29a82fac240de56e5a1a7bacd8cd31f2
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-21 10:44:41 +0600

    feat: register theme and router to my app

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
+/*widgets */
+import 'package:shoppe/core/theme/app_theme.dart';
+
+/* router*/
+import 'package:shoppe/core/router/router_generator.dart';
+
+/*constants */
+import 'package:shoppe/core/constants/app_strings.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shoppe',
-      home: Scaffold(
-        body: Center(
-          child: Text('Shoppe'),
-        ),
-      ),
+      title: AppStrings.title,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      onGenerateRoute: RouterGenerator.generateRoute,
     );
   }
 }
