e52048f06135fd5aa109bdc15aa12fe250a099bc
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-21 08:51:09 +0600

    feat: added app strings constants

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,12 @@
+abstract class AppStrings {
+  static const title = 'Shoppe';
+  static const hintEmail = 'Email or phone';
+  static const hintPass = 'Passowrd';
+  static const hintPhone = 'Phone';
+
+  /* startView */
+  static const startViewSubtitle =
+      'Beautiful eCommerce UI Kit for your online store';
+  static const startViewPrimaryButtonLable = 'Let\'s get started';
+  static const haveAccount = 'I already have an account';
+}
