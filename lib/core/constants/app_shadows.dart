341c41b7d7d6b426174f67a7708df6c9e2cbf343
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-21 10:42:00 +0600

    feat: added app shadows

---
+++ lib/core/constants/app_shadows.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+abstract class AppShadow {
+  static const BoxShadow startViewCircleShadowLight = BoxShadow(
+    blurRadius: 8,
+    offset: Offset(0, 3),
+    color: Color(0x29000000),
+    spreadRadius: 0,
+  );
+
+  static const BoxShadow startViewCircleShadowDark = BoxShadow(
+    blurRadius: 8,
+    offset: Offset(0, 3),
+    color: Color(0x29000000),
+    spreadRadius: 0,
+  );
+}
