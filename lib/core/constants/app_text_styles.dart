d7650e8a671997c681ad52101331caceab3c01a5
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-21 08:48:52 +0600

    feat: added text style

---
+++ lib/core/constants/app_text_styles.dart
@@ -0,0 +1,113 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'dart:ui';
+
+abstract class AppTextStyle {
+  /*Raleway Font */
+
+  static final ralewayBold52px = GoogleFonts.raleway(
+    fontSize: 52,
+    fontWeight: FontWeight.bold,
+    letterSpacing: -0.5199999809265137,
+    height: 1,
+  );
+
+  static final ralewayBold50px = GoogleFonts.raleway(
+    fontSize: 50,
+    fontWeight: FontWeight.bold,
+    letterSpacing: -0.5,
+    height: 5.4,
+  );
+
+  static final ralewayBold28px = GoogleFonts.raleway(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    letterSpacing: -0.28,
+    height: 3.6,
+  );
+
+  static final ralewayBold21px = GoogleFonts.raleway(
+    fontSize: 21,
+    fontWeight: FontWeight.bold,
+    letterSpacing: -0.21,
+    height: 3.0,
+  );
+
+  static final ralewayBold15px = GoogleFonts.raleway(
+    fontSize: 15,
+    fontWeight: FontWeight.bold,
+    letterSpacing: -0.15,
+    height: 1.9,
+  );
+
+  static final ralewayBold14px = GoogleFonts.raleway(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    letterSpacing: -0.14,
+    height: 1.8,
+  );
+
+  static final ralewayMedium18px = GoogleFonts.raleway(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.18,
+    height: 2.6,
+  );
+
+  static final ralewayMedium17px = GoogleFonts.raleway(
+    fontSize: 17,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.17,
+    height: 2.1,
+  );
+
+  static final ralewayMedium16px = GoogleFonts.raleway(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.16,
+    height: 2.0,
+  );
+
+  static final ralewayMedium15px = GoogleFonts.raleway(
+    fontSize: 15,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.15,
+    height: 1.9,
+  );
+
+  /*NunitoSans Font*/
+
+  static final nunitoSansBold16px = GoogleFonts.nunitoSans(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1.6,
+    height: 2.5,
+  );
+
+  static final nunitoSansLight22px = GoogleFonts.nunitoSans(
+    fontSize: 22,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0,
+    height: 3.1,
+  );
+
+  static final nunitoSansLight19px = GoogleFonts.nunitoSans(
+    fontSize: 19,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0,
+    height: 3.3,
+  );
+
+  static final nunitoSansLight15px = GoogleFonts.nunitoSans(
+    fontSize: 15,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0,
+    height: 2.6,
+  );
+
+  /*Poppins Font*/
+  static final poppinsMedium14px = GoogleFonts.poppins(
+    fontSize: 13.83,
+    fontWeight: FontWeight.w500,
+    height: 1.4,
+  );
+}
