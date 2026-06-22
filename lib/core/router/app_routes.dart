b309a7e24796711344697a7034cd84b52c3fc97b
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-22 22:02:04 +0600

    feat: register routes for login view

--- lib/core/router/app_routes.dart
+++ lib/core/router/app_routes.dart
@@ -2,4 +2,5 @@ abstract class AppRoutes {
   static const initial = '/';
   static const startView = 'startView/';
   static const createAccountView = 'createAccountView/';
+  static const loginView = 'loginView/';
 }
