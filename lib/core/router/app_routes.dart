dabd032bf8ad5811c3ba412924da681a14c508d9
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-23 20:01:30 +0600

    feat: register rotue for password Recovery View

--- lib/core/router/app_routes.dart
+++ lib/core/router/app_routes.dart
@@ -4,4 +4,5 @@ abstract class AppRoutes {
   static const createAccountView = 'createAccountView/';
   static const loginView = 'loginView/';
   static const passwordView = 'passwordView/';
+  static const passwordRecoveryView = 'passwordRecoveryView/';
 }
