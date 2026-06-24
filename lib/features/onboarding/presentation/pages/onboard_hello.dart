e62db05e380119e34278885030cb15b0091c2746
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-24 18:20:01 +0600

    Navigation from onbaord hello to onborad ready by swiping from right to left

--- lib/features/onboarding/presentation/pages/onboard_hello.dart
+++ lib/features/onboarding/presentation/pages/onboard_hello.dart
@@ -20,111 +20,118 @@ class OnboardHello extends StatelessWidget {
     final elevation = Theme.of(context).extension<AppElevationTheme>()!;
     return Scaffold(
       backgroundColor: palette.white,
-      body: Stack(
-        children: [
-          Positioned(
-            left: -140.81,
-            top: 239.16,
-            child: Transform.rotate(
-              angle: 108 * 3.1416 / 180,
+      body: GestureDetector(
+        onHorizontalDragUpdate: (details) {
+          int sensitivity = 8;
+          if (details.delta.dx < -sensitivity) {
+            Navigator.of(context).pushNamed('onboardReadyView/');
+          }
+        },
+        child: Stack(
+          children: [
+            Positioned(
+              left: -140.81,
+              top: 239.16,
+              child: Transform.rotate(
+                angle: 108 * 3.1416 / 180,
+                child: SvgPicture.asset(
+                  height: 442,
+                  width: 377,
+                  AppAssets.bubble3,
+                ),
+              ),
+            ),
+            Positioned(
+              left: -147.44,
+              top: -123.32,
               child: SvgPicture.asset(
                 height: 442,
-                width: 377,
-                AppAssets.bubble3,
+                width: 402,
+                AppAssets.bubble2,
               ),
             ),
-          ),
-          Positioned(
-            left: -147.44,
-            top: -123.32,
-            child: SvgPicture.asset(
-              height: 442,
-              width: 402,
-              AppAssets.bubble2,
-            ),
-          ),
 
-          Column(
-            mainAxisAlignment: .center,
-            children: [
-              Container(
-                width: 326,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  color: palette.white,
-                  borderRadius: BorderRadius.circular(
-                    30,
+            Column(
+              mainAxisAlignment: .center,
+              children: [
+                Container(
+                  width: 326,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: palette.white,
+                    borderRadius: BorderRadius.circular(
+                      30,
+                    ),
+                    boxShadow: [
+                      elevation.onboardHelloShadow,
+                    ],
                   ),
-                  boxShadow: [
-                    elevation.onboardHelloShadow,
-                  ],
-                ),
-                child: Column(
-                  mainAxisAlignment: .center,
-                  crossAxisAlignment: .center,
-                  mainAxisSize: .min,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(
-                          30,
+                  child: Column(
+                    mainAxisAlignment: .center,
+                    crossAxisAlignment: .center,
+                    mainAxisSize: .min,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(
+                            30,
+                          ),
+                        ),
+                        child: Image.asset(
+                          AppAssets.onboardHello,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
                         ),
                       ),
-                      child: Image.asset(
-                        AppAssets.onboardHello,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    AppGap.hXLarge,
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 42.0,
-                        right: 42,
-                        bottom: 72,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: .center,
+                      AppGap.hXLarge,
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 42.0,
+                          right: 42,
+                          bottom: 72,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: .center,
 
-                        children: [
-                          Text(
-                            AppStrings.hello,
-                            style: AppTextStyle.ralewayBold28px,
-                          ),
-                          AppGap.hMD,
-                          Text(
-                            textAlign: TextAlign.center,
-                            AppStrings.onboardHelloInfo,
-                            style: AppTextStyle.nunitoSansLight19px,
-                          ),
-                        ],
+                          children: [
+                            Text(
+                              AppStrings.hello,
+                              style: AppTextStyle.ralewayBold28px,
+                            ),
+                            AppGap.hMD,
+                            Text(
+                              textAlign: TextAlign.center,
+                              AppStrings.onboardHelloInfo,
+                              style: AppTextStyle.nunitoSansLight19px,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
 
-              AppGap.hXL,
+                AppGap.hXL,
 
-              Row(
-                mainAxisAlignment: .center,
-                children: List.generate(4, (index) {
-
-                  return AnimatedContainer(
-                    duration: Duration(seconds: 300),
-                    margin: AppPadSymmetric.hSM,
-                    height: 20,
-                    width: 20,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: palette.primary,
-                    ),
-                  );
-                }),
-              ),
-            ],
-          ),
-        ],
+                Row(
+                  mainAxisAlignment: .center,
+                  children: List.generate(4, (index) {
+                    return AnimatedContainer(
+                      duration: Duration(seconds: 300),
+                      margin: AppPadSymmetric.hSM,
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: palette.primary,
+                      ),
+                    );
+                  }),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
