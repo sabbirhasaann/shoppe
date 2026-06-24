373eb4f8c3d75cd0be5f0bb6fbdbabcb70861206
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-24 17:54:15 +0600

    register onboard hello shadow

--- lib/core/theme/app_elevation_theme.dart
+++ lib/core/theme/app_elevation_theme.dart
@@ -4,20 +4,32 @@ import 'package:shoppe/core/constants/app_shadows.dart';
 
 class AppElevationTheme extends ThemeExtension<AppElevationTheme> {
   final List<BoxShadow> cardShadow;
+  final BoxShadow onboardHelloShadow;
 
-  const AppElevationTheme({required this.cardShadow});
+  const AppElevationTheme({
+    required this.cardShadow,
+    required this.onboardHelloShadow,
+  });
 
   factory AppElevationTheme.light() => const AppElevationTheme(
     cardShadow: [AppShadow.startViewCircleShadowLight],
+    onboardHelloShadow: AppShadow.onboardHelloShadow,
   );
 
   factory AppElevationTheme.dark() => const AppElevationTheme(
     cardShadow: [AppShadow.startViewCircleShadowDark],
+    onboardHelloShadow: AppShadow.onboardHelloShadow,
   );
 
   @override
-  AppElevationTheme copyWith({List<BoxShadow>? cardShadow}) {
-    return AppElevationTheme(cardShadow: cardShadow ?? this.cardShadow);
+  AppElevationTheme copyWith({
+    List<BoxShadow>? cardShadow,
+    BoxShadow? onboardShadowHello,
+  }) {
+    return AppElevationTheme(
+      cardShadow: cardShadow ?? this.cardShadow,
+      onboardHelloShadow: onboardHelloShadow,
+    );
   }
 
   @override
@@ -26,6 +38,13 @@ class AppElevationTheme extends ThemeExtension<AppElevationTheme> {
     return AppElevationTheme(
       cardShadow:
           BoxShadow.lerpList(cardShadow, other.cardShadow, t) ?? cardShadow,
+      onboardHelloShadow:
+          BoxShadow.lerp(
+            onboardHelloShadow,
+            other.onboardHelloShadow,
+            t,
+          ) ??
+          onboardHelloShadow, // 3. Added null safety handling fallback for lerp
     );
   }
 }
