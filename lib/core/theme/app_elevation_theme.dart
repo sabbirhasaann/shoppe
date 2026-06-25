ed07277d620a1281564a37a61137b959b43d201e
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-25 09:18:51 +0600

    register bottomNavigation bar elevation

--- lib/core/theme/app_elevation_theme.dart
+++ lib/core/theme/app_elevation_theme.dart
@@ -5,30 +5,37 @@ import 'package:shoppe/core/constants/app_shadows.dart';
 class AppElevationTheme extends ThemeExtension<AppElevationTheme> {
   final List<BoxShadow> cardShadow;
   final BoxShadow onboardHelloShadow;
+  final BoxShadow bottomNavigationBarShadow;
 
   const AppElevationTheme({
     required this.cardShadow,
     required this.onboardHelloShadow,
+    required this.bottomNavigationBarShadow,
   });
 
   factory AppElevationTheme.light() => const AppElevationTheme(
     cardShadow: [AppShadow.startViewCircleShadowLight],
     onboardHelloShadow: AppShadow.onboardHelloShadow,
+    bottomNavigationBarShadow: AppShadow.navigationBarShadow,
   );
 
   factory AppElevationTheme.dark() => const AppElevationTheme(
     cardShadow: [AppShadow.startViewCircleShadowDark],
     onboardHelloShadow: AppShadow.onboardHelloShadow,
+    bottomNavigationBarShadow: AppShadow.navigationBarShadow,
   );
 
   @override
   AppElevationTheme copyWith({
     List<BoxShadow>? cardShadow,
     BoxShadow? onboardShadowHello,
+    BoxShadow? bottomNavigationBarShadow,
   }) {
     return AppElevationTheme(
       cardShadow: cardShadow ?? this.cardShadow,
       onboardHelloShadow: onboardHelloShadow,
+      bottomNavigationBarShadow:
+          bottomNavigationBarShadow ?? this.bottomNavigationBarShadow,
     );
   }
 
@@ -45,6 +52,13 @@ class AppElevationTheme extends ThemeExtension<AppElevationTheme> {
             t,
           ) ??
           onboardHelloShadow, // 3. Added null safety handling fallback for lerp
+      bottomNavigationBarShadow:
+          BoxShadow.lerp(
+            bottomNavigationBarShadow,
+            other.bottomNavigationBarShadow,
+            t,
+          ) ??
+          bottomNavigationBarShadow,
     );
   }
 }
